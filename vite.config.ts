5a2975d55499bdcf9cdf971b9f5c5832dfb028b5
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-28 20:44:29 +0900

    open消す

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        open: true, // 自動でブラウザを開く
+        //open: true, // 自動でブラウザを開く
         port: 3000, // ポート番号
         host: true, // ネットワーク上のIPアドレスを表示
     },
